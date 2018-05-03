4735a468a2d19de93bbe2d42b29b45ba1e14d57b
raspberrypi9999 <sifriede@uni-mainz.de>
2018-05-03 15:02:11 +0200

    clone branch 3.14 from epics-base github

--- epics_scripts/01_epics_download_github.sh
+++ epics_scripts/01_epics_download_github.sh
@@ -2,7 +2,7 @@
 . env.sh
 
 #===== EPICS, asyn, extensionsTOP and StreamDevice from github =====
-git clone https://github.com/epics-base/epics-base $EPICS_HOME/base
+git clone https://github.com/epics-base/epics-base -b 3.14 $EPICS_HOME/base
 git clone https://github.com/epics-modules/asyn $EPICS_HOME/asyn
 git clone https://github.com/epics-extensions/extensions $EPICS_HOME/extensions
 git clone https://github.com/paulscherrerinstitute/StreamDevice $EPICS_HOME/StreamDevice
